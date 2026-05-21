61398b8f8a73107f5fb15a9c8ba1faea7ccc3bb9
Llopis Viñolas, Ivan <ivan.llopis@sogeti.com>
2026-05-21 20:29:20 +0200

    refactor: consnption to consumption

--- components/admin/ConsumptionList.tsx
+++ components/admin/ConsumptionList.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { addConsumption } from "@/app/actions/consption"
+import { addConsumption } from "@/app/actions/consumption"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 
